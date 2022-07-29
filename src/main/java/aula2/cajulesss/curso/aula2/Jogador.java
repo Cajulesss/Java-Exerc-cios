2fcb2125d8fc4665523d60a1ac498d5c2ef3a933
Cajulesss <jilesss9023Caju@outlook.com>
2022-07-29 12:39:18 -0300

    gitignore

--- src/main/java/aula2/cajulesss/curso/aula2/Jogador.java
+++ src/main/java/aula2/cajulesss/curso/aula2/Jogador.java
@@ -9,5 +9,49 @@ package aula2.cajulesss.curso.aula2;
  * @author xuxu
  */
 public class Jogador {
+    //Atributos:
     
+    private String nome;
+    private int gols;
+    public int assistencias;
+    
+    //Construtor
+    public Jogador(String recebe_nome,int recebe_gols,int nume_assis){
+        this.setNome(recebe_nome);
+        this.setGols(recebe_gols);
+        this.Numero_de_Assistencias(nume_assis);
+    }
+    
+    
+    //Métodos Acessores
+    public String getNome(){
+        return this.nome;
+    }
+    
+    public void setNome(String recebe_nome){
+        this.nome = recebe_nome;
+    }
+    
+    public int getGols(){
+        return this.gols;
+    }
+    
+    public void setGols(int recebe_gols){
+        this.gols = recebe_gols;
+    }
+    
+    public void Status_da_partida(){
+        System.out.println("Nome do Jogador: " + this.getNome());
+        System.out.println("Número de gols: " + this.getGols());
+    }
+    //Métodos
+    
+    /*public void Numero_de_Gols(int gol){
+        this.gols = gol;
+    }*/
+    
+    public void Numero_de_Assistencias(int nume_assis){
+        System.out.println("Número de assistências recebidas:"+nume_assis);
+        this.assistencias = nume_assis;
+    }
 }
--- src/main/java/aula2/cajulesss/curso/aula2/Jogo_Futebol.java
+++ src/main/java/aula2/cajulesss/curso/aula2/Jogo_Futebol.java
@@ -8,13 +8,29 @@ package aula2.cajulesss.curso.aula2;
  *
  * @author xuxu
  */
+//import java.util.Scanner;
 public class Jogo_Futebol {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Criar uma classe Jogador ue tenha as propriedades nome,gols,assistências.E os métodos adicionarGol e adicionarAssistencia que receba como argumento as quantidades de gols/assistências
+
+        //Scanner resposta_usuario = new Scanner(System.in);
+        Jogador primeira_partida_futebol = new Jogador("Jullyene",1,67);
+        primeira_partida_futebol.Status_da_partida();
+        System.out.println("\n");
+        
+        /*partidas_futebol.setNome("junior");
+        partidas_futebol.setGols(2);*/
+        
+        Jogador segunda_partida_futebol = new Jogador("Igor",3,45);
+        segunda_partida_futebol.Status_da_partida();
+        
+        
+        
+        
     }
     
 }
